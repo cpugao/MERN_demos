ce180b20af717d8bfb535c7090c5e8bc072c0643
Neil M <neilm813@gmail.com>
2020-09-03 12:09:17 -0700

    validation form

--- w1/cra-intro/src/App.js
+++ w1/cra-intro/src/App.js
@@ -4,10 +4,11 @@ import "./App.css";
 import Counter from "./components/Counter";
 import SingleQuote from "./components/SingleQuote";
 import QuoteList from "./components/QuoteList";
+import ValidationForm from "./components/ValidationForm";
 
 function App() {
   return (
-    <div className="App" style={{ border: "1px solid red" }}>
+    <div className="App">
       <h1>Hello from create react app</h1>
 
       <h3>Quotes List 1</h3>
