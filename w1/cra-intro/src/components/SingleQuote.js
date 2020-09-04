0052718b48015cb6d52ded8666d6e953d91259e5
Neil M <neilm813@gmail.com>
2020-09-04 11:07:04 -0700

    create quote

--- w1/cra-intro/src/components/SingleQuote.js
+++ w1/cra-intro/src/components/SingleQuote.js
@@ -7,7 +7,17 @@ import styles from "./SingleQuote.module.css";
 const SingleQuote = (props) => {
   console.log(props);
 
-  return <q className={`${styles.italic} ${styles.bold}`}>{props.children}</q>;
+  return (
+    <div>
+      <q
+        style={{ color: props.quote.color }}
+        className={`${styles.italic} ${styles.bold}`}
+      >
+        {props.quote.text}
+      </q>{" "}
+      - <span>{props.quote.author}</span>
+    </div>
+  );
 };
 
 export default SingleQuote;
