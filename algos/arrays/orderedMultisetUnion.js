75c98c97b51276e91398616ee5f05041e7d49168
Neil M <neilm813@gmail.com>
2020-09-09 09:13:30 -0700

    algo slns

--- algos/arrays/orderedMultisetUnion.js
+++ algos/arrays/orderedMultisetUnion.js
@@ -28,9 +28,62 @@ const expected = [1, 2, 2, 2, 6, 6, 7];
  *         An ordered multiset union of @sortedA and @sortedB
  *         The return should include dupes, but the amount of dupes for each int should be based on
  *         the max amount that dupe appears from one set, not the combined amount from both sets.
- * Time:   O(...)
- * Space:  O(...)
+ * Time:   O(n) linear, where n = longest length
+ * Space:  O(n + m) where n = sortedA.length, m = sortedB.length because if there are no dupes all will be kept from both
  */
-function orderedMultisetUnion(sortedA, sortedB) {}
+function orderedMultisetUnion(sortedA, sortedB) {
+  const res = [];
+  let idx1 = 0,
+    idx2 = 0;
+
+  while (idx1 < sortedA.length || idx2 < sortedB.length) {
+    if (idx1 === sortedA.length) {
+      // sortedB is longer, push in all remaining sortedB nums
+      res.push(sortedB[idx2++]);
+      continue;
+    } else if (idx2 === sortedB.length) {
+      // sortedA is longer, push in remaining sortedA nums
+      res.push(sortedA[idx1++]);
+      continue;
+    }
+
+    if (sortedA[idx1] === sortedB[idx2]) {
+      res.push(sortedA[idx1++]);
+      idx2++; // since both were same, increment both
+    } else if (sortedA[idx1] < sortedB[idx2]) {
+      res.push(sortedA[idx1++]);
+    } else {
+      res.push(sortedB[idx2++]);
+    }
+  }
+  return res;
+}
+
+function orderedMultisetUnion2(sortedA, sortedB) {
+  let idx1 = 0,
+    idx2 = 0;
+  const ret = [],
+    len1 = sortedA.length,
+    len2 = sortedB.length;
+
+  while (idx1 < len1 && idx2 < len2) {
+    const n1 = sortedA[idx1],
+      n2 = sortedB[idx2];
+
+    if (n1 === n2) {
+      ret.push(n1);
+      idx1++;
+      idx2++;
+    } else if (n1 < n2) {
+      ret.push(n1);
+      idx1++;
+    } else {
+      ret.push(n2);
+      idx2++;
+    }
+  }
+  // arrays might be different lengths, if any elems are remaining, concat them
+  return ret.concat(sortedA.slice(idx1)).concat(sortedB.slice(idx2));
+}
 
 module.exports = { orderedMultisetUnion };
