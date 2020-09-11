c1336b009a50581305a94c62108aa2f7f1756be4
Neil M <neilm813@gmail.com>
2020-09-11 14:59:21 -0700

    express modularization p2

--- cities-demo/server/routes/city.routes.js
+++ cities-demo/server/routes/city.routes.js
@@ -3,4 +3,8 @@ const cityController = require("../controllers/city.controller");
 module.exports = (app) => {
   // in DJango: path("api/cities", views.create_city)
   app.post("/api/cities", cityController.create);
+  app.get("/api/cities", cityController.getAll);
+  app.get("/api/cities/:id", cityController.getOne);
+  app.delete("/api/cities/:id", cityController.delete);
+  app.put("/api/cities/:id", cityController.update);
 };
